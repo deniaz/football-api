4ccb14a35de49a562da33f058b8a6b1c69630311
deniaz <robert@cheekyowl.com>
2014-07-10 20:48:02 +0200

    Moved scraper to bin (CLI Script) and implemented basic version of JSON API

--- server.js
+++ server.js
@@ -15,93 +15,66 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
     res.render('index');
 });
+
 app.get('/teams', function(req, res) {
-    request(cfg.services.team_base_uri, function(error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
-            var teams = [];
+    var db = mongojs.connect(cfg.db.getUri(), ['teams']);
 
-            $('#team-index .team').each(function() {
-                var team = {};
-                team.name = $(this).find('.team-name').text();
-                team.link = $(this).find('a').attr('href');
-                teams.push(team);
-            });
-
-            console.log(teams);
-
-            var db = mongojs.connect(cfg.db.getUri(), ['teams']);
-
-            teams.forEach(function(team) {
-                db.teams.findAndModify({
-                    query: {
-                        name: team.name
-                    },
-                    update: team,
-                    upsert: true
-                });
-            });
-
-            res.send('Fetched teams: ' + teams.length);
+    db.teams.find({}).toArray(function(err, teams) {
+        if (err) {
+            res.send(500);
         }
+
+        res.json(200, teams);
     });
 });
-app.get('/players', function(req, res) {
-    var db = mongojs.connect(cfg.db.getUri()),
-        teamCollection = db.collection('teams'),
-        playerCollection = db.collection('players');
 
-    teams = teamCollection.find({}, function(error, docs) {
-        docs.forEach(function(team) {
-            console.info('Going for team ' + team.name);
-            var uri = cfg.services.base_uri + team.link;
-            request(uri, function(error, response, html) {
-                if (!error) {
-                    var $ = cheerio.load(html);
+app.get('/teams/:team', function(req, res) {
+    var db = mongojs.connect(cfg.db.getUri(), ['teams']);
 
-                    $('#team tr').each(function() {
+    db.teams.findOne({ slug: req.params.team }, function(err, team) {
+        if (err) {
+            res.send(500);
+        }
 
-                        var player = {};
-                        player.squadNumber = $($(this).find('td')[0]).text().trim();
-                        player.fullName = $($(this).find('td')[1]).find('a').text().trim();
-                        player.shirtName = $($(this).find('td')[1]).find('span').text().trim();
-                        player.position = $($(this).find('td')[2]).find('span').text().trim();
-                        player.birthdate = $($(this).find('td')[3]).text().trim();
-                        player.nationality = $($(this).find('td')[4]).find('span').text().trim();
-                        player.appeareances = $($(this).find('td')[5]).text().trim();
-                        player.goals = $($(this).find('td')[6]).text().trim();
-                        player.assists = $($(this).find('td')[7]).text().trim();
-                        player.yellowCards = $($(this).find('td')[8]).text().trim();
-                        player.redCards = $($(this).find('td')[9]).text().trim();
+        res.json(200, team);
+    });
+});
 
-                        player.link = $($(this).find('td')[1]).find('a').attr('href');
-                        player.team = team.name;
+app.get('/teams/:team/players', function(req, res) {
+    var db = mongojs.connect(cfg.db.getUri(), ['teams', 'players']);
 
-                        if (!!player.shirtName && !!player.position) {
-                            playerCollection.findAndModify({
-                                query: {
-                                    squadNumber: player.squadNumber,
-                                    fullName: player.fullName,
-                                    team: player.team
-                                },
-                                update: player,
-                                upsert: true
-                            });
-                        }
-                    });
+    db.teams.findOne({ slug: req.params.team }, function(err, team) {
+        if (err) {
+            res.send(500);
+        }
 
-                    console.log('Stored all players.');
-                } else {
-                    console.error(error);
-                }
-            });
+        db.players.find({ team: team.name }).toArray(function(err, players) {
+            if (err) {
+                res.send(500);
+            }
+
+            res.json(200, players);
         });
     });
-
-    res.send('Fetched players from: ' + teams.length);
 });
-//app.get('/matches', function(req, res) {});
+
+app.get('/teams/:team/players/:player', function(req, res) {
+    var db = mongojs.connect(cfg.db.getUri(), ['teams', 'players']);
+
+    db.teams.findOne({ slug: req.params.team }, function(err, team) {
+        if (err) {
+            res.send(500);
+        }
+
+        db.players.findOne({ slug: req.params.player, team: team.name }, function(err, player) {
+            if (err) {
+                res.send(500);
+            }
+
+            res.json(200, player);
+        });
+    });
+});
 
 app.listen('9999');
-console.log('Scrape Server started');
 exports = module.exports = app;
